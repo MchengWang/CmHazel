d5ceef79683dc9e62c7fcbd7a5a4c32af72d186b
MchengWang <2734190606@qq.com>
2025-12-31 16:18:31 +0800

    Starting the rendering journey -- the first triangle

--- CmHazel/source/Platform/OpenGL/OpenGLContext.cpp
+++ CmHazel/source/Platform/OpenGL/OpenGLContext.cpp
@@ -19,6 +19,11 @@ namespace CmHazel
 		glfwMakeContextCurrent(m_WindowHandle);
 		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 		CM_CORE_ASSERT(status, "Failed to intialized Glad");
+
+		CM_CORE_INFO("OpenGL Info: ");
+		CM_CORE_INFO("	Vendor: {0}", (char*)glGetString(GL_VENDOR));
+		CM_CORE_INFO("	Renderer: {0}", (char*)glGetString(GL_RENDERER));
+		CM_CORE_INFO("	Version: {0}", (char*)glGetString(GL_VERSION));
 	}
 
 	void OpenGLContext::SwapBuffers()
