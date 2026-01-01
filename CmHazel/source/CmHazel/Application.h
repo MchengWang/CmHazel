015898a7ca646ac6bb174706f879bb76afb0e294
MchengWang <2734190606@qq.com>
2026-01-01 12:25:05 +0800

    The first shader of the rendering journey

--- CmHazel/source/CmHazel/Application.h
+++ CmHazel/source/CmHazel/Application.h
@@ -9,6 +9,8 @@
 
 #include "CmHazel/ImGui/ImGuiLayer.h"
 
+#include "CmHazel/Renderer/Shader.h"
+
 namespace CmHazel
 {
 
@@ -38,6 +40,7 @@ namespace CmHazel
 		LayerStack m_LayerStack;
 
 		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+		std::unique_ptr<Shader> m_Shader;
 
 	private:
 		static Application* s_Instance;
