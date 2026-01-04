3f30d0b383ff92dfe7de9915bb5f85b84dd187ea
MchengWang <2734190606@qq.com>
2026-01-04 16:31:34 +0800

    Renderer2D can now use a single shader to render both textured and non-textured quads

--- CmHazel/source/CmHazel/Renderer/Texture.cpp
+++ CmHazel/source/CmHazel/Renderer/Texture.cpp
@@ -7,12 +7,24 @@
 namespace CmHazel
 {
 
+	Shared<Texture2D> Texture2D::Create(uint32_t width, uint32_t height)
+	{
+		switch (Renderer::GetAPI())
+		{
+		case RendererAPI::API::None: CM_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
+		case RendererAPI::API::OpenGL: return CreateShared<OpenGLTexture2D>(width, height);
+		}
+
+		CM_CORE_ASSERT(false, "Unkown RendererAPI!");
+		return nullptr;
+	}
+
 	Shared<Texture2D> Texture2D::Create(const std::string& path)
 	{
 		switch (Renderer::GetAPI())
 		{
 		case RendererAPI::API::None: CM_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
-		case RendererAPI::API::OpenGL: return std::make_shared<OpenGLTexture2D>(path);
+		case RendererAPI::API::OpenGL: return CreateShared<OpenGLTexture2D>(path);
 		}
 
 		CM_CORE_ASSERT(false, "Unkown RendererAPI!");
