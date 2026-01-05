8d91c15b877e5b55f344554ab9377132642a5d6c
MchengWang <2734190606@qq.com>
2026-01-05 17:16:13 +0800

    count the number of renders in rendering

--- Sandbox/source/Sandbox2D.cpp
+++ Sandbox/source/Sandbox2D.cpp
@@ -30,6 +30,7 @@ void Sandbox2D::OnUpdate(CmHazel::Timestep ts)
 	m_CameraController.OnUpdate(ts);
 
 	// ‰÷»æ
+	CmHazel::Renderer2D::ResetStats();
 	{
 		CM_PROFILE_SCOPE("Renderer Prep");
 		CmHazel::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
@@ -43,11 +44,22 @@ void Sandbox2D::OnUpdate(CmHazel::Timestep ts)
 		CM_PROFILE_SCOPE("Renderer Draw");
 		CmHazel::Renderer2D::BeginScene(m_CameraController.GetCamera());
 		// CmHazel::Renderer2D::DrawQuad(m_CameraController.GetCamera().GetPosition(), {0.8f, 0.8f}, {0.8f, 0.2f, 0.3f, 1.0f});
-		CmHazel::Renderer2D::DrawRotatedQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, -45.0f, {0.8f, 0.2f, 0.3f, 1.0f});
+		CmHazel::Renderer2D::DrawRotatedQuad({ 1.0f, 0.0f }, { 0.8f, 0.8f }, -45.0f, { 0.8f, 0.2f, 0.3f, 1.0f });
 		CmHazel::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
 		CmHazel::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, { 0.2f, 0.3f, 0.8f, 1.0f });
-		CmHazel::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture, 10.0f);
+		CmHazel::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 20.0f, 20.0f }, m_CheckerboardTexture, 10.0f);
 		CmHazel::Renderer2D::DrawRotatedQuad({ -2.0f, 0.0f, 0.0f }, { 1.0f, 1.0f }, rotation, m_CheckerboardTexture, 20.0f);
+		CmHazel::Renderer2D::EndScene();
+
+		CmHazel::Renderer2D::BeginScene(m_CameraController.GetCamera());
+		for (float y = -5.0f; y < 5.0f; y += 0.5f)
+		{
+			for (float x = -5.0f; x < 5.0f; x += 0.5f)
+			{
+				glm::vec4 color = { (x + 5.0f) / 10.0f, 0.4f, (y + 5.0f) / 10.0f, 0.7f };
+				CmHazel::Renderer2D::DrawQuad({ x, y }, { 0.45f, 0.45f }, color);
+			}
+		}
 		CmHazel::Renderer2D::EndScene();
 	}
 }
@@ -55,8 +67,15 @@ void Sandbox2D::OnUpdate(CmHazel::Timestep ts)
 void Sandbox2D::OnImGuiRender()
 {
 	ImGui::Begin("Settings");
-	ImGui::ColorEdit4("Square Color", glm::value_ptr(m_SquareColor));
 
+	auto stats = CmHazel::Renderer2D::GetStats();
+	ImGui::Text("Renderer2D Stats:");
+	ImGui::Text("Draw Calls: %d", stats.DrawCalls);
+	ImGui::Text("Quads : %d", stats.QuadCount);
+	ImGui::Text("Vertices : %d", stats.GetTotalVertexCount());
+	ImGui::Text("Indices : %d", stats.GetTotalIndexCount());
+
+	ImGui::ColorEdit4("Square Color", glm::value_ptr(m_SquareColor));
 	ImGui::End();
 }
 
