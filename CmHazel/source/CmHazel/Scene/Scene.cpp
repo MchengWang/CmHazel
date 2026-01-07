c47ed8c4630081d8e94cc89514a068b7b5019229
MchengWang <2734190606@qq.com>
2026-01-07 14:19:30 +0800

    the transform component now stores translation, rotation, and scale instead of a transformation matrix

--- CmHazel/source/CmHazel/Scene/Scene.cpp
+++ CmHazel/source/CmHazel/Scene/Scene.cpp
@@ -48,7 +48,7 @@ namespace CmHazel
 
 		// Render 2D
 		Camera* mainCamera = nullptr;
-		glm::mat4* cameraTransform = nullptr;
+		glm::mat4 cameraTransform;
 
 		{
 
@@ -58,21 +58,21 @@ namespace CmHazel
 						if (camera.Primary)
 						{
 							mainCamera = &camera.Camera;
-							cameraTransform = &transform.Transform;
+							cameraTransform = transform.GetTransform();
 						}
 					});
 		}
 
 		if (mainCamera)
 		{
-			Renderer2D::BeginScene(*mainCamera, *cameraTransform);
+			Renderer2D::BeginScene(*mainCamera, cameraTransform);
 
 			auto group = m_Registry.group<TransformComponent>(entt::get<SpriteRendererComponent>);
 			for (auto entity : group)
 			{
 				auto [transform, sprite] = group.get<TransformComponent, SpriteRendererComponent>(entity);
 
-				Renderer2D::DrawQuad(transform, sprite.Color);
+				Renderer2D::DrawQuad(transform.GetTransform(), sprite.Color);
 			}
 
 			Renderer2D::EndScene();
