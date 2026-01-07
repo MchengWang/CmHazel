66570c1fe48d44e2647c2716ba974b8d9ccf5a30
MchengWang <2734190606@qq.com>
2026-01-07 20:05:14 +0800

    add ImGuizmo

--- CmHazel/source/CmHazel/ImGui/ImGuiLayer.cpp
+++ CmHazel/source/CmHazel/ImGui/ImGuiLayer.cpp
@@ -10,6 +10,8 @@
 #include <GLFW/glfw3.h>
 #include <glad/glad.h>
 
+#include "ImGuizmo.h"
+
 namespace CmHazel
 {
 
@@ -85,6 +87,7 @@ namespace CmHazel
 		ImGui_ImplOpenGL3_NewFrame();
 		ImGui_ImplGlfw_NewFrame();
 		ImGui::NewFrame();
+		ImGuizmo::BeginFrame();
 	}
 
 	void ImGuiLayer::End()
