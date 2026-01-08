51c5635a0dc40e632a25959eaf4acbfdcc2c64a9
MchengWang <2734190606@qq.com>
2026-01-08 14:22:36 +0800

    added GL_RED_INTEGER as a framebuffer format and provided mouse position picking

--- CmEditor/source/EditorLayer.cpp
+++ CmEditor/source/EditorLayer.cpp
@@ -27,7 +27,7 @@ namespace CmHazel
 		m_CheckerboardTexture = Texture2D::Create("assets/textures/Checkerboard.png");
 
 		FramebufferSpecification fbSpec;
-		fbSpec.Attachments = { FramebufferTextureFormat::RGBA8, FramebufferTextureFormat::Depth };
+		fbSpec.Attachments = { FramebufferTextureFormat::RGBA8, FramebufferTextureFormat::RED_INTEGER, FramebufferTextureFormat::Depth };
 		fbSpec.Width = 1280;
 		fbSpec.Height = 720;
 		m_Framebuffer =  Framebuffer::Create(fbSpec);
@@ -127,6 +127,20 @@ namespace CmHazel
 		// 更新场景
 		m_ActiveScene->OnUpdateEditor(ts, m_EditorCamera);
 
+		auto [mx, my] = ImGui::GetMousePos();
+		mx -= m_ViewportBounds[0].x;
+		my -= m_ViewportBounds[0].y;
+		glm::vec2 viewportSize = m_ViewportBounds[1] - m_ViewportBounds[0];
+		my = viewportSize.y - my;
+		int mouseX = (int)mx;
+		int mouseY = (int)my;
+
+		if (mouseX >= 0 && mouseY >= 0 && mouseX < (int)viewportSize.x && mouseY < (int)viewportSize.y)
+		{
+			int pixelData = m_Framebuffer->ReadPixel(1, mouseX, mouseY);
+			CM_CORE_WARN("Pixel data = {0}", pixelData);
+		}
+
 		m_Framebuffer->Unbind();
 	}
 
@@ -190,7 +204,7 @@ namespace CmHazel
 			{
 				// 关闭全屏可以让窗口移动到其他窗口的前面，
 				// 目前如果没有更细的窗口深度 / Z轴控制，我们无法逆转。
-			    // ImGui：：MenuItem（“Fullscreen”，NULL，opt_fullscreen_persistant）;
+				// ImGui：：MenuItem（“Fullscreen”，NULL，opt_fullscreen_persistant）;
 
 				if (ImGui::MenuItem("New", "Ctrl + N"))
 					NewScene();
@@ -218,23 +232,33 @@ namespace CmHazel
 		ImGui::Text("Quads: %d", stats.QuadCount);
 		ImGui::Text("Vertices: %d", stats.GetTotalVertexCount());
 		ImGui::Text("Indices: %d", stats.GetTotalIndexCount());
-		
+
 		ImGui::End();
 
 		ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2{ 0, 0 });
 		ImGui::Begin("Viewport");
+		auto viewportOffset = ImGui::GetCursorPos(); // 包括任务栏
 
 		m_ViewportFocused = ImGui::IsWindowFocused();
 		m_ViewportHovered = ImGui::IsWindowHovered();
 		Application::Get().GetImGuiLayer()->BlockEvents(!m_ViewportFocused && !m_ViewportHovered);
 
 		ImVec2 viewportPanelSize = ImGui::GetContentRegionAvail();
-		
+
 		m_ViewportSize = { viewportPanelSize.x, viewportPanelSize.y };
 
 		uint64_t textureID = m_Framebuffer->GetColorAttachmentRendererID();
 		ImGui::Image(reinterpret_cast<void*>(textureID), ImVec2{ m_ViewportSize.x, m_ViewportSize.y }, ImVec2{ 0, 1 }, ImVec2{ 1, 0 });
-		
+
+		auto windowSize = ImGui::GetWindowSize();
+		ImVec2 minBound = ImGui::GetWindowPos();
+		minBound.x += viewportOffset.x;
+		minBound.y += viewportOffset.y;
+
+		ImVec2 maxBound = { minBound.x + windowSize.x, minBound.y + windowSize.y };
+		m_ViewportBounds[0] = { minBound.x, minBound.y };
+		m_ViewportBounds[1] = { maxBound.x, maxBound.y };
+
 		// Gizmos
 		Entity selectedEntity = m_SceneHierarchyPanel.GetSelectedEntity();
 		if (selectedEntity && m_GizmoType != -1)
