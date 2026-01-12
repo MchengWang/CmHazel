3512197f958baa40128e3926b3c929ef1b8910b5
MchengWang <2734190606@qq.com>
2026-01-12 16:44:16 +0800

    CopyComponent and CopyComponentIfExists as a variadic template functions

--- CmHazel/source/CmHazel/Scene/Components.h
+++ CmHazel/source/CmHazel/Scene/Components.h
@@ -162,4 +162,14 @@ namespace CmHazel
 		CircleCollider2DComponent(const CircleCollider2DComponent&) = default;
 	};
 
+	template <typename... Component>
+	struct ComponentGroup
+	{
+	};
+
+	using AllComponents =
+		ComponentGroup<TransformComponent, SpriteRendererComponent,
+		CircleRendererComponent, CameraComponent, NativeScriptComponent,
+		Rigidbody2DComponent, BoxCollider2DComponent, CircleCollider2DComponent>;
+
 }
